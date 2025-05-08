58dac7f2070401b39cd2b6666a2bd9b8a97ae8d2
Jesse <15653378+squarezw@user.noreply.gitee.com>
2025-05-08 23:13:17 +0800

    enable port

--- my-aiavatar-web/src/server.cjs
+++ my-aiavatar-web/src/server.cjs
@@ -37,6 +37,6 @@ app.post('/upload', upload.single('file'), (req, res) => {
 app.use(express.static('dist'));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
