84a7b931370616e0cd6cf7f5672c3f74c88daee8
Jesse <15653378+squarezw@user.noreply.gitee.com>
2025-05-09 18:26:20 +0800

    using '/web' as sub dir on domian

--- aiavatar-web/vite.config.js
+++ aiavatar-web/vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    allowedHosts: 'all'
-  }
+  base: '/web/',
+  plugins: [react()]
 })
