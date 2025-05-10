884824ee5ac6290e250ab080c44e382f56c7b15a
Jesse <15653378+squarezw@user.noreply.gitee.com>
2025-05-10 11:35:29 +0800

    Updated upload dir

--- aiavatar-server/config.py
+++ aiavatar-server/config.py
@@ -1,7 +1,6 @@
 import os
 
-UPLOAD_DIR = '/usr/share/nginx/html/face2face/';
-#UPLOAD_DIR = '../shared_data/face2face/';
+UPLOAD_DIR = '../shared_data/face2face/';
 UPLOAD_FOLDER = os.path.join(UPLOAD_DIR,'temp');
 
 LOG_FILE = os.path.join(UPLOAD_DIR, 'log/dh.log')
