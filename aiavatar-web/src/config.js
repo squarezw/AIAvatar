8018b9a12ab8c69767b3c119824192cededd4bfb
Jesse <15653378+squarezw@user.noreply.gitee.com>
2025-05-26 15:04:50 +0800

    Updated api endpoints.

--- aiavatar-web/src/config.js
+++ aiavatar-web/src/config.js
@@ -1,2 +1,4 @@
 // 全局 API 地址配置
-export const API_BASE_URL = `${window.location.protocol}//${window.location.hostname}`; 
+export const API_BASE_URL = `https://www.voicecan.online`;
+export const API_VIDEO_URL = `https://api-video.voicecan.online`;
+export const API_FILE_URL = `https://api-file.voicecan.online`;
